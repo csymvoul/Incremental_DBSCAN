cd7ad8edbc65606fb473fb8fe0a2f28c66b12faa
Chrysostomos Symvoulidis <chrisimvou@gmail.com>
2019-02-05 16:58:17 +0200

    minor changes

--- rabbitmq/consumer.py
+++ rabbitmq/consumer.py
@@ -1,9 +1,11 @@
 import pika
-from incremental_dbscan import initiate_dbscan
+# from incremental_dbscan import initiate_dbscan
+
+rabbitmq_ip = open('rabbitmq_ip', 'r')
 
 
 connection = pika.BlockingConnection(pika.ConnectionParameters(
-    '83.212.238.159'))
+    rabbitmq_ip.read()))
 channel = connection.channel()
 
 # Declare the channel on the specific queue
@@ -12,12 +14,12 @@ channel.queue_declare(queue='hello')
 
 def callback(ch, method, properties, body):
     print("[x] Received %r" % body)
-    initiate_dbscan(body)
+    # initiate_dbscan(body)
 
 
 channel.basic_consume(callback,
                       queue='hello',
                       no_ack=True)
 
-print("[*] Waiting for messages. To exit press CTRL+C")
+print("[x] Waiting for messages. To exit press CTRL+C")
 channel.start_consuming()
