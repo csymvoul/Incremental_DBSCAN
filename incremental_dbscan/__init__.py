b6342ebdec15b293c082c9e254822ed5347c56b0
Chrysostomos Symvoulidis <chrisimvou@gmail.com>
2019-02-13 13:04:27 +0200

    find mean_core_elements for every cluster created and store them in a dataframe

--- incremental_dbscan/__init__.py
+++ incremental_dbscan/__init__.py
@@ -11,6 +11,7 @@ class Incremental_DBSCAN:
         self.dataset = pd.DataFrame(columns=['CPU', 'Memory', 'Disk'])
         self.labels = pd.DataFrame(columns=['Label'])
         self.final_dataset = pd.DataFrame(columns=['CPU', 'Memory', 'Disk', 'Label'])
+        self.mean_core_elements = pd.DataFrame(columns=['CPU', 'Memory', 'Disk', 'Label'])
 
     def set_data(self, message):
         # store the collected message to a temp dataframe
@@ -28,16 +29,19 @@ class Incremental_DBSCAN:
         n_clusters_ = len(set(self.labels)) - (1 if -1 in self.labels else 0)
         # TODO  Still not fully working -- Needs to be  checked
         self.add_labels_to_dataset(batch_dbscan.labels_)
+        self.sort_dataset_based_on_labels()
+        self.find_mean_core_element()
 
     def add_labels_to_dataset(self, labels):
-        self.labels = pd.DataFrame(labels, columns=['Labels'])
-        # final_ds =
-        # final_ds =
+        self.labels = pd.DataFrame(labels, columns=['Label'])
         self.final_dataset = pd.concat([self.dataset, self.labels], axis=1)
-        print('printing final_dataset')
+
+    def sort_dataset_based_on_labels(self):
+        self.final_dataset = self.final_dataset.sort_values(by=['Label'])
+        self.final_dataset = self.final_dataset.astype(int)
         print(self.final_dataset)
 
-    def find_mean_element(self):
-        # TODO find them mean element of each dataset in order to
-        # TODO compare the distances on each dataset
-        return self.dataset
+    def find_mean_core_element(self):
+        self.mean_core_elements = self.final_dataset.groupby('Label')['CPU', 'Memory', 'Disk'].mean()
+        print(self.mean_core_elements)
+
