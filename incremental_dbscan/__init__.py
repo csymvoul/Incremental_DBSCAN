792b7c3d498938d869625d52d4f49c88d3f55348
Chrysostomos Symvoulidis <chrisimvou@gmail.com>
2019-02-19 15:25:17 +0200

    Find minimun Euclidean distance of the current element and the available mean core elements

--- incremental_dbscan/__init__.py
+++ incremental_dbscan/__init__.py
@@ -2,10 +2,15 @@ import pandas as pd
 import io
 from sklearn.cluster import DBSCAN
 from math import sqrt
+from math import pow
 
 
-def distance(element, mean_core_elements):
-    return 0
+# Returns the Euclidean of a mean core element and the current element
+def distance(element, mean_core_element):
+    distance = ((element['CPU'] - mean_core_element['CPU'])**2 +
+                    (element['Memory'] - mean_core_element['Memory'])**2 +
+                    (element['Disk'] - mean_core_element['Disk'])**2)**(1/2)
+    return distance.iloc[0].astype(float)
 
 
 class Incremental_DBSCAN:
@@ -30,7 +35,11 @@ class Incremental_DBSCAN:
         # Get the number of the clusters created
         n_clusters_ = len(set(self.labels)) - (1 if -1 in self.labels else 0)
         self.add_labels_to_dataset(batch_dbscan.labels_)
-        self.sort_dataset_based_on_labels()
+
+        # Cast everything in the final_dataset as integer.
+        # If this line is missing, it throws an error
+        self.final_dataset = self.final_dataset.astype(int)
+        # self.sort_dataset_based_on_labels()
         self.find_mean_core_element()
 
     def add_labels_to_dataset(self, labels):
@@ -38,7 +47,7 @@ class Incremental_DBSCAN:
         self.final_dataset = pd.concat([self.dataset, self.labels], axis=1)
 
     def sort_dataset_based_on_labels(self):
-        print(self.final_dataset)
+        # print(self.final_dataset)
         self.final_dataset = self.final_dataset.sort_values(by=['Label'])
         # Cast everything in the final_dataset as integer.
         # If this line is missing, it throws an error
@@ -50,27 +59,26 @@ class Incremental_DBSCAN:
         # Find the mean core elements of each cluster
         self.mean_core_elements = self.mean_core_elements \
             .groupby('Label')['CPU', 'Memory', 'Disk'].mean()
-        print(self.mean_core_elements)
+        # print(self.mean_core_elements)
         response = self.calculate_distance()
-        print(response)
 
     def calculate_distance(self):
-        print(self.final_dataset.tail(n=1))
         min_dist = None
         min_dist_index = None
         # Check if there are elements in the core_elements dataframe.
         # In other words if there are clusters created by the DBSCAN algorithm
         if not self.mean_core_elements.empty:
             # Iterate over the mean_core_elements dataframe and find the minimum distance
-            for index, row in self.mean_core_elements.iterrows():
+            for index, current_mean_core_element in self.mean_core_elements.iterrows():
                 tmp_dist = distance(element=self.final_dataset.tail(n=1),
-                                                    mean_core_elements=row)
+                                    mean_core_element=current_mean_core_element)
                 if min_dist is None:
                     min_dist = tmp_dist
                     min_dist_index = index
                 elif tmp_dist < min_dist:
                     min_dist = tmp_dist
                     min_dist_index = index
+            print('Minimum distance is: ', min_dist, ' at cluster ', min_dist_index)
             return min_dist_index
         else:
             return 'Empty dataframe, no clusters found'
